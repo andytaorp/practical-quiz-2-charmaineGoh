c789ae8918a5c508609e985ea20d9ef756e3cee9
charmaineGoh <23021210@myrp.edu.sg>
2024-11-27 16:18:55 +0800

    u

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,7 +6,7 @@ function Task({ task, onToggleTask, onDeleteTask }) {
       <input
         type="checkbox"
         checked={task.completed}
-        onChange={() => onToggleTask(task.id)}
+        onChange={() => onToggleTask(task.id,{ ...task, completed: !task.completed })}
       />
       <span style={{ textDecoration: task.completed ? "line-through" : "none" }}>
         
